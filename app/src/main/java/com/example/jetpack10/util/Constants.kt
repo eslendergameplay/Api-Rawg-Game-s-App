47dc3889d3e605c94d98f9dab794050f901317ce
Juan Diego Serrano Monsalve <eslendergameplayy@hotmail.com>
2024-08-29 21:43:21 -0400

    Update Constants.kt

--- app/src/main/java/com/example/jetpack10/util/Constants.kt
+++ app/src/main/java/com/example/jetpack10/util/Constants.kt
@@ -7,7 +7,7 @@ class Constants {
     companion object{
         const val BASE_URL = "https://api.rawg.io/api/"
         const val ENDPOINT = "games"
-        const val API_KEY = "?key=5cc1ec96fef14809a814b47d11075b42"
+        const val API_KEY = "?key=Tu llave para api"
         val Color_Back = Color(0xFF2B2626)
     }
 
